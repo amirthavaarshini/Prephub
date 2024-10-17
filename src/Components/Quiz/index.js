c918626d7152d1a239c82563d16568294a51cb92
unknown <vaarshiniamirtha@gmail.com>
2024-10-17 12:44:07 +0530

    toggle show and hide button and reloading the page

--- src/Components/Quiz/index.js
+++ src/Components/Quiz/index.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const Quiz = () => {
+
+
+    const [showAns, setShowAns] = useState(false);
+
+    const handleShowBtn = () => {
+        setShowAns(prev => !prev);
+    }
+
+    const handleNxt = () => {
+        window.location.reload();
+    }
 
     return (
         <>
             <div className="quiz-container">
                 <div className="quiz-qstn">What is React?</div>
                 <div className="text-container">
-                    <textarea rows="5"  className="quiz-text" placeholder="Type your Answer here."></textarea>
+                    <textarea rows="5" className="quiz-text" placeholder="Type your Answer here."></textarea>
                 </div>
                 <div className="show-hide-container">
-                    <button className="show-btn">Show Answer</button>
+                    <button className="show-btn" onClick={handleShowBtn}>
+                        {showAns === true ?"Hide Answer" : "Show Answer"}
+                    </button>
                 </div>
-                <div className="answer-container">It is a a development server that uses Webpack to compile React, JSX, and ES6, auto-prefix CSS files.</div>
-                <div className="show-hide-container">
-                    <button className="show-btn">Next Question</button>
-                </div>
+                {showAns &&
+                    <>
+                        <div className="answer-container" >It is a a development server that uses Webpack to compile React, JSX, and ES6, auto-prefix CSS files.</div>
+                        <div className="show-hide-container">
+                            <button className="show-btn" onClick={handleNxt}>Next Question</button>
+                        </div>
+                    </>
+                }
             </div>
 
         </>
