f20938f150d984081026759c2e0d18b3a9e385e9
unknown <vaarshiniamirtha@gmail.com>
2024-10-17 11:36:47 +0530

    navigate to quiz page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./index.css"; 
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    const goToNewPage = () => {
+        navigate("/quiz");
+    }
 
     return(
         <>
         <div className="home-container">
-            <button  className="home-btn">Start Quiz</button>
+            <button  className="home-btn" onClick={() => goToNewPage()}>Start Quiz</button>
         </div>
         </>
     )
